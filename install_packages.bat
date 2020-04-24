30d60e12aa35ec76304e77de8164cd26138ea802
Alexander Shender <aka.sova@gmail.com>
2020-04-24 13:32:35 +0300

    upload hw_1 original files

---
+++ install_packages.bat
@@ -0,0 +1,14 @@
+
+
+echo "Installing packages"
+
+python -m pip install --upgrade pip --user
+
+pip install --upgrade numpy          --user
+pip install --upgrade scipy          --user
+pip install --upgrade matplotlib     --user
+pip install --upgrade typing         --user
+pip install --upgrade dataclasses    --user
+pip install --upgrade networkx       --user
+
+pause
