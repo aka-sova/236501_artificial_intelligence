6f2e8e09592827d9efcf884101d09113cf9918a0
Alexander Shender <aka.sova@gmail.com>
2020-05-17 23:08:09 +0300

    solve 1-7 dry

--- hw_1/other/permutations_calc.py
+++ hw_1/other/permutations_calc.py
@@ -2,7 +2,7 @@
 
 import math
 
-k_vals = list([5, 8 , 9, 10, 11, 15])
+k_vals = list([10, 13, 15, 16, 17, 20, 21, 24])
 calc_speed = 2**30
 
 pos_paths = []
@@ -11,13 +11,13 @@ calc_time_s = []
 
 path_calc = lambda k : math.factorial(k)
 
-
-for k in k_vals:
-    pos_paths.append(path_calc(k))
-    pos_paths_log.append(pos_paths[-1])
-    calc_time_s.append(pos_paths[-1] / calc_speed)
-    print(f"k = {k} , possible paths = {pos_paths[-1]} , \
-        log2 = {pos_paths_log[-1]}, calc time = {round(calc_time_s[-1],10)} [s]")
+with open("out.txt", "w") as f: 
+	for k in k_vals:
+		pos_paths.append(path_calc(k))
+		pos_paths_log.append(math.log2(pos_paths[-1]))
+		calc_time_s.append(pos_paths[-1] / calc_speed)
+		f.write(f"k = {k} , possible paths = {pos_paths[-1]} , \
+			log2 = {pos_paths_log[-1]}, calc time = {round(calc_time_s[-1],10)} [s]\n")
 
 
 
