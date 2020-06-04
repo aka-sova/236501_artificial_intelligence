358a879913bd5d0632e75a889d00ff4382d9cd1a
Alexander Shender <aka.sova@gmail.com>
2020-06-04 20:38:01 +0300

    add the fixed ordered alpha beta agent

--- hw_2/code/OrderedAlphaBetaPlayer_old.py
+++ hw_2/code/OrderedAlphaBetaPlayer_old.py
@@ -6,7 +6,7 @@ from GeneralPlayer import State, GeneralPlayer
 
 
 
-class OrderedAlphaBetaPlayer(GeneralPlayer):
+class OrderedAlphaBetaPlayer_old(GeneralPlayer):
     def __init__(self):
         super().__init__()
         self.state = None        
@@ -16,8 +16,6 @@ class OrderedAlphaBetaPlayer(GeneralPlayer):
         self.heuristics_used = 0
         self.branches_pruned = 0
 
-        self.lastMinimaxValues = {}
-
     def make_move(self, time_limit) -> tuple:  # time parameter is not used, we assume we have enough time.
 
         """Implement the RB - MiniMax algorithm with iterative deepening"""
@@ -26,7 +24,6 @@ class OrderedAlphaBetaPlayer(GeneralPlayer):
 
         prev_loc = self.state.my_loc
         self.state.board[prev_loc] = -1
-        self.lastMinimaxValues = {}
 
 
         # time = 1000
@@ -39,8 +36,7 @@ class OrderedAlphaBetaPlayer(GeneralPlayer):
         # DEPTH = 1
         print(f"Depth : {current_depth}")
         self.leaves_developed = 0
-        (best_new_move, max_value ) = self.rb_ordered_alphabeta(self.state, DecidingAgent = "Me",\
-             D = current_depth, Alpha = float('-inf'), Beta = float('inf'), isRoot = True)
+        (best_new_move, max_value ) = self.rb_ordered_alphabeta(self.state, DecidingAgent = "Me", D = current_depth, Alpha = float('-inf'), Beta = float('inf'))
         best_move_so_far = best_new_move
 
         print(f"Move value : {max_value}")
@@ -61,9 +57,7 @@ class OrderedAlphaBetaPlayer(GeneralPlayer):
             self.leaves_developed = 0
             self.heuristics_used = 0
 
-            (best_new_move, max_value ) = self.rb_ordered_alphabeta(self.state, DecidingAgent = "Me", \
-                D = current_depth, Alpha = float('-inf'), Beta = float('inf'),\
-                    isRoot=True)
+            (best_new_move, max_value ) = self.rb_ordered_alphabeta(self.state, DecidingAgent = "Me", D = current_depth, Alpha = float('-inf'), Beta = float('inf'))
             best_move_so_far = best_new_move
 
             print(f"Move value : {max_value}")
@@ -93,7 +87,7 @@ class OrderedAlphaBetaPlayer(GeneralPlayer):
 
 
 
-    def rb_ordered_alphabeta(self, CurrentState : State,  DecidingAgent : str, D : int, Alpha: int, Beta: int, isRoot : bool) -> tuple:
+    def rb_ordered_alphabeta(self, CurrentState : State,  DecidingAgent : str, D : int, Alpha: int, Beta: int) -> tuple:
         """Get the next most beneficial move
 
         CurrentState = TUPLE(BOARD, MY_LOC, ENEMY_LOC)
@@ -112,22 +106,24 @@ class OrderedAlphaBetaPlayer(GeneralPlayer):
 
 
         if D == 0:
-            heuristic_value = self.get_heuristic_value(CurrentState, DecidingAgent)
+            # heuristic_value = self.get_heuristic_value(CurrentState, DecidingAgent)
+            heuristic_value = self.get_heuristic_value(CurrentState, "Me")
             self.heuristics_used += 1
             self.leaves_developed +=1
             return (None, heuristic_value)
 
         # get all the children states
         children_moves = self.get_children(CurrentState, DecidingAgent)
+        child_states = []
 
-        if isRoot:           
+        for child_move in children_moves:
+            # update the state (board, locations) after a child move is made
+            ChildState = copy.deepcopy(CurrentState)
+            ChildState.update(child_move, DecidingAgent)
+            child_states.append(ChildState)
 
-            # reorder the moves according to the lastMinimaxValues
-            if self.lastMinimaxValues != {}:
-                ordered_children_moves_dict = dict(sorted(self.lastMinimaxValues.items(), key = lambda kv:(kv[1])))
-                children_moves = list(ordered_children_moves_dict.keys())
-                children_moves.reverse() # we want to open the moves with BIGGERST minimax value first
 
+        child_states = self.order_children(child_states, DecidingAgent)
         
         if DecidingAgent == "Me":
             # MAX
@@ -135,14 +131,11 @@ class OrderedAlphaBetaPlayer(GeneralPlayer):
             CurMax = float('-inf')
             CurMaxMove = None
 
-            for child_move in children_moves:
 
-                # update the state (board, locations) after a child move is made
-                ChildState = copy.deepcopy(CurrentState)
-                ChildState.update(child_move, DecidingAgent)
+            for ChildState in child_states:
                 
                 
-                _, max_child_value = self.rb_ordered_alphabeta(ChildState, "Enemy", D-1, Alpha, Beta, False)
+                _, max_child_value = self.rb_ordered_alphabeta(ChildState, "Enemy", D-1, Alpha, Beta)
                 # print(f"D = {D} , max_child_value =  {str(max_child_value)}")
                 if max_child_value > CurMax:
                     CurMax = max_child_value
@@ -153,11 +146,6 @@ class OrderedAlphaBetaPlayer(GeneralPlayer):
                 if CurMax >= Beta:
                     self.branches_pruned += 1
                     return (None, float('inf'))
-
-                if isRoot:
-                    #  root will always open all its children
-                    # will always start with Me
-                    self.lastMinimaxValues[child_move] = max_child_value
             
             return (CurMaxMove, CurMax)
 
@@ -168,14 +156,9 @@ class OrderedAlphaBetaPlayer(GeneralPlayer):
             CurMin = float("inf")
             CurMinMove = None
 
-            for child_move in children_moves:
+            for ChildState in child_states:
                 
-                # update the state (board, locations) after a child move is made
-                ChildState = copy.deepcopy(CurrentState)
-                ChildState.update(child_move, DecidingAgent)
-
-                
-                _, min_child_value = self.rb_ordered_alphabeta(ChildState, "Me", D-1, Alpha, Beta, False)
+                _, min_child_value = self.rb_ordered_alphabeta(ChildState, "Me", D-1, Alpha, Beta)
                 if min_child_value < CurMin:
                     CurMin = min_child_value
                     CurMinMove = child_move
@@ -187,3 +170,25 @@ class OrderedAlphaBetaPlayer(GeneralPlayer):
                     return (None, float('-inf'))
             
             return (CurMinMove, CurMin)
+
+
+    def order_children(self, children_states, DecidingAgent)  -> list:
+
+        # order children according to Heuristic
+        children_dict = {}
+
+        for ChildState in children_states:
+            child_heuristic = self.get_heuristic_value(ChildState, DecidingAgent)
+            children_dict[ChildState] = child_heuristic
+
+        # put highest heuristic first - I maximize
+        ordered_children_states_dict = dict(sorted(children_dict.items(), key = lambda kv:(kv[1])))
+
+        ordered_children_states_list = list(ordered_children_states_dict.keys())
+
+        if DecidingAgent == "Me":
+            # Enemy minimizes - put highest heuristic first
+            # invert the sort order
+            ordered_children_states_list.reverse()
+
+        return ordered_children_states_list
